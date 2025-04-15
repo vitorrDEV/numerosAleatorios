0fcd59fd147e8d5055a934c31433d95ef47b838a
Vitor Mendes <vitorrmendes45@gmail.com>
2025-04-14 21:09:24 -0300

    projeto inicial

---
+++ logica-js-aula_2/app.js
@@ -0,0 +1,26 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let chute;
+console.log(chute);
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    }else if (chute > numeroSecreto){
+        alert(`O numero secreto é menor que ${chute}`);
+    }else {
+        alert(`O numero secreto é maior que ${chute}`);
+    }
+
+    tentativas++
+
+}
+
+let quantasTentativas = tentativas > 1 ? "tentativas" : "tentativa"
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${quantasTentativas}`);
+
